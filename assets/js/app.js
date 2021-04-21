82324eb84bffeb80dc41ada20af50acfd84215aa
Jeffrey18 <geraldinojeffrey1@gmail.com>
2021-04-21 17:36:51 +0800

    Removing elements from the DOM

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,16 @@
 //variables 
-const tweetList = document.getElementById('tweet-List');
+const tweetList = document.getElementById('tweet-list');
 
 
 
 //Event Listeners
-
+eventListerners();
 function eventListerners(){
 
  document.querySelector('#form').addEventListener('submit', newTweet);
+
+ // Remove tweet from the list
+ tweetList.addEventListener('click', removeTweet);
 
 }
 
@@ -15,12 +18,33 @@ function eventListerners(){
 //Functions
 function newTweet(e){
 e.preventDefault();
+
+
 // read the textarea value
 const tweet = document.getElementById('tweet').value;
+
+// create the  remove button
+const removeBtn = document.createElement('a');
+removeBtn.classList = 'remove-tweet';
+removeBtn.textContent = 'X';
+
+
 // create an <li> element
 const li = document.createElement('li');
 li.textContent = tweet;
-tweetList.appendChild(li);
 
+
+// Add the remove button to each tweet 
+li.appendChild(removeBtn);
+
+// Add to the List
+tweetList.appendChild(li);
 }
 
+//Removes tweet from the DOM
+function removeTweet(e){
+    if(e.target.classList.contains('remove-tweet')){
+        e.target.parentElement.remove();
+    }
+
+}
